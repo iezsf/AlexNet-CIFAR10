e82fa7f05a3ac77d340a3b92fa5fae7123867013
PhanVinhLong <longpv.se@gmail.com>
2019-08-05 09:26:40 +0700

    Add something

--- alexnet-full.py
+++ alexnet-full.py
@@ -42,6 +42,9 @@ Using ``torchvision``, it’s extremely easy to load CIFAR10.
 import torch
 import torchvision
 import torchvision.transforms as transforms
+import os 
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
 
 ########################################################################
 # The output of torchvision datasets are PILImage images of range [0, 1].
@@ -51,12 +54,12 @@ transform = transforms.Compose(
     [transforms.ToTensor(),
      transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
-trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+trainset = torchvision.datasets.CIFAR10(root=dir_path + '\\data', train=True,
                                         download=True, transform=transform)
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
                                           shuffle=True, num_workers=2)
 
-testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+testset = torchvision.datasets.CIFAR10(root=dir_path + '\\data', train=False,
                                        download=True, transform=transform)
 testloader = torch.utils.data.DataLoader(testset, batch_size=4,
                                          shuffle=False, num_workers=2)
